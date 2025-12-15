53f8d89268024265da49755cd5fa17d67cd9e865
alejandra <abarreto@360pmi.com>
2025-12-15 18:44:18 -0500

    updates

--- src/components/GaleriaCarrusel.jsx
+++ src/components/GaleriaCarrusel.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, A11y } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-export default function GaleriaCarrusel({ imagenes }) {
+export default function GaleriaCarrusel({ imagenes, titulo }) {
   return (
-    <div className="w-full h-64 relative group">
+    <div className="w-full h-full relative group">
       <Swiper
         modules={[Navigation, Pagination, A11y]}
         spaceBetween={0}
@@ -19,7 +18,12 @@ export default function GaleriaCarrusel({ imagenes }) {
       >
         {imagenes.map((img, index) => (
           <SwiperSlide key={index}>
-            <img src={img} alt={`Foto ${index}`} className="w-full h-full object-cover" loading="lazy"/>
+            <img 
+              src={img} 
+              alt={titulo ? `${titulo} - Foto ${index + 1}` : `Foto ${index + 1}`} 
+              className="w-full h-full object-cover" 
+              loading="lazy"
+            />
           </SwiperSlide>
         ))}
       </Swiper>
